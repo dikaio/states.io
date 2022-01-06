eb43ba2bd4dce56fc71341b4a91f9922ce2bef32
UXDX <d@dika.io>
2022-01-06 12:20:36 -0800

    Update

--- app/root.tsx
+++ app/root.tsx
@@ -4,25 +4,89 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix'
+import type { LinksFunction, MetaFunction } from 'remix'
+import { Link } from 'remix'
+import tailwind from './app.css'
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+  const title = `States`
+  const description = `...`
+  return {
+    title,
+    description,
+    'twitter:image': 'https://states.io/social.png',
+    'twitter:card': 'summary_large_image',
+    'twitter:creator': '@StatesHQ',
+    'twitter:site': '@StatesHQ',
+    'twitter:title': title,
+    'twitter:description': description,
+  }
+}
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: '/favicon.svg',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png',
+    },
+    {
+      rel: 'icon',
+      sizes: 'any',
+      href: '/favicon.ico',
+    },
+    {
+      rel: 'manifest',
+      href: '/site.webmanifest',
+    },
+    {
+      rel: 'mask-icon',
+      href: '/safari-pinned-tab.svg',
+      color: '#8454E3',
+    },
+    {
+      rel: 'stylesheet',
+      href: tailwind,
+    },
+  ]
+}
+
 
 export default function App() {
+  const currentYear = new Date().getFullYear()
   return (
-    <html lang="en">
+    <html lang="en"
+      className="h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="flex flex-col h-full antialiased bg-gray-50 dark:bg-dark">
+        <div className="flex-auto">
+          <Outlet />
+        </div>
+        <footer className="shrink-0">
+          <div className='container mx-auto prose text-center dark:prose-invert'>
+            <div className='flex justify-between'>
+              <p>
+              </p>
+              <p>
+                &copy;{' '}{currentYear}{' '}<Link to="/">States</Link>{' · '}<Link to="/terms">Terms of Use</Link>{' · '}<Link to="/privacy">Privacy Policy</Link>
+              </p>
+              <p>
+              </p>
+            </div>
+          </div>
+        </footer>
         <ScrollRestoration />
         <Scripts />
         {process.env.NODE_ENV === "development" && <LiveReload />}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,19 @@
+import { Link } from 'remix'
+import { motion } from "framer-motion"
+import { Logo } from '~/components/logo'
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <>
+      <motion.div
+        animate={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 20 }}
+        exit={{ opacity: 0, y: -20 }}
+        transition={{ duration: 0.15 }}
+        className="flex items-center justify-center h-full mx-auto prose prose-slate lg:prose-lg dark:prose-invert">
+        <Link to="/">
+          <h1>States</h1>
+        </Link>
+      </motion.div>
+    </>
+  )
 }
